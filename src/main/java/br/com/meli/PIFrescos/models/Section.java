e21ee0b047c6b9e14212f08c60ff8802508648e6
a-preis <ana.preis@mercadolivre.com>
2022-04-22 14:53:15 -0300

    crete entity warehouse/section/inboundorder

---
+++ src/main/java/br/com/meli/PIFrescos/models/Section.java
@@ -0,0 +1,24 @@
+package br.com.meli.PIFrescos.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer sectionCode;
+    private StorageType storageType;
+    private Integer maxCapacity;
+    private Integer currentCapacity;
+}
---
+++ src/main/java/br/com/meli/PIFrescos/models/Warehouse.java
@@ -0,0 +1,13 @@
+package br.com.meli.PIFrescos.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warehouse {
+
+    private Integer warehouseCode;
+}
