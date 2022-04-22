6cdc51305cea5370d517cfe2c6684d6e915f7ca0
Julio César Gama <juliocesar.gama@mercadolivre.com>
2022-04-22 14:32:39 -0300

    Initial commit

---
+++ src/main/java/br/com/meli/PIFrescos/PiFrescosApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.PIFrescos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiFrescosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiFrescosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meli/PIFrescos/PiFrescosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.PIFrescos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiFrescosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
