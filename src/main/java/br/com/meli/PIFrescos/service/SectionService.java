952b631208503f89eae5b02838b8e2981859d909
a-preis <ana.preis@mercadolivre.com>
2022-04-22 16:18:54 -0300

    add description to methods

--- src/main/java/br/com/meli/PIFrescos/service/SectionService.java
+++ src/main/java/br/com/meli/PIFrescos/service/SectionService.java
@@ -5,7 +5,6 @@ import br.com.meli.PIFrescos.repository.SectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.function.Supplier;
 
 /**
  * @author Ana Preis
@@ -17,18 +16,28 @@ public class SectionService {
     @Autowired
     SectionRepository sectionRepository;
 
+    /**
+     * @author Ana Preis
+     * Cria uma nova section do warehouse
+     */
     public Section createSection(Section section) {
         return sectionRepository.save(section);
     }
 
-    public Integer updateCapacity(Integer sectionCode, Integer newCapacity){
+    /**
+     * @author Ana Preis
+     * Recebe a id de uma section e a quantidade a ser adicionada ao currentCapacity
+     * Verifica se a section existe e se, ao adicionar a quantidade nova, não ultrapassa o maxCapacity.
+     * Se estiver tudo certo, atualiza o currentCapacity.
+     */
+    public Integer updateCapacity(Integer sectionCode, Integer quantity){
         Section section = sectionRepository.findById(sectionCode).get();
         if (section == null){
             throw new RuntimeException("A section não existe");
         }
 
-        if (section.getCurrentCapacity() + newCapacity < section.getMaxCapacity()) {
-            section.setCurrentCapacity(section.getCurrentCapacity() + newCapacity);
+        if (section.getCurrentCapacity() + quantity < section.getMaxCapacity()) {
+            section.setCurrentCapacity(section.getCurrentCapacity() + quantity);
             return section.getCurrentCapacity();
         }
         throw new RuntimeException("A capacidade do setor não suporta essa order");
