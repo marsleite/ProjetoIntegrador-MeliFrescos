4d84c291aa83c9db70c02bf125844af33b3f1140
Hugo Freire <ahugo.freire@gmail.com>
2022-04-22 15:23:15 -0300

    feat: add SectionRepository

---
+++ src/main/java/br/com/meli/PIFrescos/repository/SectionRepository.java
@@ -0,0 +1,12 @@
+package br.com.meli.PIFrescos.repository;
+
+import br.com.meli.PIFrescos.models.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Antonio Hugo
+ */
+@Repository
+public interface SectionRepository extends JpaRepository<Section, Integer> {
+}
