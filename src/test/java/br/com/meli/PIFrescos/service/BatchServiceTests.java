b0b5e7c1ea318cb339d08fa4baa9383a4d42b905
Hugo Freire <ahugo.freire@gmail.com>
2022-04-25 15:43:32 -0300

    test: add tests to BatchService

--- src/test/java/br/com/meli/PIFrescos/service/BatchServiceTests.java
+++ src/test/java/br/com/meli/PIFrescos/service/BatchServiceTests.java
@@ -1,12 +1,23 @@
 package br.com.meli.PIFrescos.service;
 
+import br.com.meli.PIFrescos.models.Batch;
+import br.com.meli.PIFrescos.models.InboundOrder;
+import br.com.meli.PIFrescos.models.Product;
+import br.com.meli.PIFrescos.models.StorageType;
 import br.com.meli.PIFrescos.repository.BatchRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class BatchServiceTests {
@@ -17,16 +28,71 @@ public class BatchServiceTests {
     @Mock
     private BatchRepository batchRepository;
 
-    @Mock
-    private ProductService productService;
 
+    Product mockProduct = new Product();
+    InboundOrder mockInboundOrder = new InboundOrder();
     @BeforeEach
-    public void init() {
-        batchService = new BatchServiceImpl(batchRepository, productService);
+    public void setUp() {
+        mockProduct.setProductId(1);
+        mockProduct.setProductName("Test");
+        mockProduct.setProductDescription("Product Test");
+        mockProduct.setProductType(StorageType.FRESH);
     }
 
     @Test
-    public void shouldBeAbleCreateBatchAndReturnTheCreatedBatch(){
+    public void shouldBeAbleReturnBatch(){
+
+        Batch mockBatch = Batch.builder()
+                .batchNumber(1)
+                .currentTemperature(7.0f)
+                .minimumTemperature(6.0f)
+                .initialQuantity(5)
+                .currentQuantity(5)
+                .manufacturingDate(LocalDate.now())
+                .dueDate(LocalDate.now())
+                .inboundOrder(mockInboundOrder)
+                .product(mockProduct)
+                .build();
+
+        Mockito.when(batchRepository.findById(ArgumentMatchers.any())).thenReturn(java.util.Optional.of(mockBatch));
+
+        Batch result = batchService.findBatchById(1);
+
+        Assertions.assertEquals(mockBatch, result);
+
+    }
+
+    @Test
+    public void shouldNotBeAbleToFindBatchIfDoesntExist() {
+
+        Assertions.assertThrows(RuntimeException.class, () -> batchService.findBatchById(100));
+
+    }
+
+    @Test
+    public void shouldBeAbleReturnBatchList(){
+        List listMockBatch = new ArrayList();
+
+        Batch mockBatch = Batch.builder()
+                .batchNumber(1)
+                .currentTemperature(7.0f)
+                .minimumTemperature(6.0f)
+                .initialQuantity(5)
+                .currentQuantity(5)
+                .manufacturingDate(LocalDate.now())
+                .dueDate(LocalDate.now())
+                .inboundOrder(mockInboundOrder)
+                .product(mockProduct)
+                .build();
+
+        listMockBatch.add(mockBatch);
+        listMockBatch.add(mockBatch);
+
+        Mockito.when(batchRepository.findBatchesByProduct_ProductId(ArgumentMatchers.any())).thenReturn(listMockBatch);
+
+        List<Batch> result = batchService.findBatchesByProduct(1);
+
+        Assertions.assertEquals(listMockBatch, result);
 
     }
 }
