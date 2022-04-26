3c1f964fed481030f205597d53f1c8ba01f93f93
a-preis <ana.preis@mercadolivre.com>
2022-04-26 17:15:23 -0300

    create PurchaseOrder service and fix UserForm

--- src/main/java/br/com/meli/PIFrescos/repository/PurchaseOrderRepository.java
+++ src/main/java/br/com/meli/PIFrescos/repository/PurchaseOrderRepository.java
@@ -1,8 +1,11 @@
 package br.com.meli.PIFrescos.repository;
 
+import br.com.meli.PIFrescos.models.InboundOrder;
 import br.com.meli.PIFrescos.models.PurchaseOrder;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * @author Ana Preis
