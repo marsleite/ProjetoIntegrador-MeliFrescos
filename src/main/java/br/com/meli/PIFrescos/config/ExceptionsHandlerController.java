e364409e486f3292d426e81af1a3008d75addaf7
julisouza <julianoalcione@gmail.com>
2022-04-26 09:28:40 -0300

    Criacao exception handler

---
+++ src/main/java/br/com/meli/PIFrescos/config/ExceptionsHandlerController.java
@@ -0,0 +1,55 @@
+package br.com.meli.PIFrescos.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ExceptionsHandlerController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormularioDto> formsError(MethodArgumentNotValidException exception) {
+        List<ErroDeFormularioDto> dto = new ArrayList<>();
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e -> {
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErroDeFormularioDto erro = new ErroDeFormularioDto(e.getField(), mensagem);
+            dto.add(erro);
+        });
+
+        return dto;
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({RuntimeException.class, EntityNotFoundException.class})
+    public String handleError(RuntimeException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+}
