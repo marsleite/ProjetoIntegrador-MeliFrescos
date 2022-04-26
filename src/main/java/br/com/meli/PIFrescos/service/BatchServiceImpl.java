31ee7f2b989892d7564f782ad5b1ab91a1206d02
Hugo Freire <ahugo.freire@gmail.com>
2022-04-26 10:26:27 -0300

    refactor: add author in methods

--- src/main/java/br/com/meli/PIFrescos/service/BatchServiceImpl.java
+++ src/main/java/br/com/meli/PIFrescos/service/BatchServiceImpl.java
@@ -8,7 +8,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * @author Antonio Hugo
+ */
 @Service
 @AllArgsConstructor
 public class BatchServiceImpl implements IBatchService {
