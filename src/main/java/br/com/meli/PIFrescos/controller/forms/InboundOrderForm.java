ebe07538f20afb8d419911f1587b39294cbbd805
Hugo Freire <ahugo.freire@gmail.com>
2022-04-27 18:30:07 -0300

    fix: move InboundOrderDTO to controller/forms and rename to InboundOrderForm

--- src/main/java/br/com/meli/PIFrescos/controller/forms/InboundOrderForm.java
+++ src/main/java/br/com/meli/PIFrescos/controller/forms/InboundOrderForm.java
@@ -1,10 +1,13 @@
-package br.com.meli.PIFrescos.controller.dtos;
+package br.com.meli.PIFrescos.controller.forms;
 
+import br.com.meli.PIFrescos.controller.dtos.BatchDTO;
 import br.com.meli.PIFrescos.models.InboundOrder;
 import br.com.meli.PIFrescos.models.Section;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,13 +19,14 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class InboundOrderDTO {
+public class InboundOrderForm {
 
+    @NotEmpty(message = "orderDate field can't be empty")
     private LocalDate orderDate;
     private Section section;
     private List<BatchDTO> batchStock;
 
-    public static InboundOrder convert(InboundOrderDTO dto) {
+    public static InboundOrder convert(InboundOrderForm dto) {
         return new InboundOrder(null, dto.getOrderDate(), dto.getSection(),
                 BatchDTO.convert(dto.getBatchStock()));
     }
