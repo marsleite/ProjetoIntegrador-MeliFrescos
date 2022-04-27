88853d7fcb825f6cf70d59d6aaf274e70feec5e1
Hugo Freire <ahugo.freire@gmail.com>
2022-04-27 17:00:20 -0300

    feat: add method getProductsByOrderId

--- src/main/java/br/com/meli/PIFrescos/controller/PurchaseOrderController.java
+++ src/main/java/br/com/meli/PIFrescos/controller/PurchaseOrderController.java
@@ -1,22 +1,23 @@
 package br.com.meli.PIFrescos.controller;
 
+import br.com.meli.PIFrescos.controller.dtos.OrderProductDTO;
 import br.com.meli.PIFrescos.controller.dtos.TotalPriceDTO;
 import br.com.meli.PIFrescos.controller.forms.PurchaseOrderForm;
+import br.com.meli.PIFrescos.models.Batch;
+import br.com.meli.PIFrescos.models.Product;
 import br.com.meli.PIFrescos.models.PurchaseOrder;
 import br.com.meli.PIFrescos.service.PurchaseOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/fresh-products/orders")
 public class PurchaseOrderController {
 
     @Autowired
@@ -24,12 +25,13 @@ public class PurchaseOrderController {
 
     /**
      * Insere nova compra e retorna o valor total do pedido.
+     *
      * @return TotalPriceDTO
      * @author Julio César Gama
      */
 
     @PostMapping("")
-    public ResponseEntity<TotalPriceDTO> postOrder(@RequestBody PurchaseOrderForm form){
+    public ResponseEntity<TotalPriceDTO> postOrder(@RequestBody PurchaseOrderForm form) {
 
         PurchaseOrder order = form.convertToEntity();
         PurchaseOrder savedOrder = service.save(order);
@@ -37,5 +39,17 @@ public class PurchaseOrderController {
         BigDecimal totalPrice = service.calculateTotalPrice(savedOrder);
 
         return new ResponseEntity<TotalPriceDTO>(new TotalPriceDTO(totalPrice), HttpStatus.CREATED);
+    }
+
+    /**
+     * Este endpoint retorna todos os produtos de um pedido.
+     * @param querytype
+     * @return  OrderProductDTO
+     * @author Antonio Hugo
+     */
+    @GetMapping("")
+    public ResponseEntity<OrderProductDTO> getProductsByOrderId(@RequestParam Integer querytype) {
+           List<Product> products = service.findProductsByOrderId(querytype);
+        return ResponseEntity.ok().body(new OrderProductDTO(querytype, products));
     }
 }
