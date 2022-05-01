7a28b932d091955199843c0fa05d9947effc5050
Hugo Freire <ahugo.freire@gmail.com>
2022-05-01 18:23:02 -0300

    test: add integration tests PurchaseOrderController

--- src/test/java/br/com/meli/PIFrescos/integrationtests/PurchaseOrderControllerIT.java
+++ src/test/java/br/com/meli/PIFrescos/integrationtests/PurchaseOrderControllerIT.java
@@ -1,11 +1,11 @@
 package br.com.meli.PIFrescos.integrationtests;
 
-import br.com.meli.PIFrescos.config.security.TokenService;
-import br.com.meli.PIFrescos.controller.forms.PurchaseOrderForm;
+import br.com.meli.PIFrescos.controller.dtos.TokenDto;
 import br.com.meli.PIFrescos.models.*;
-import br.com.meli.PIFrescos.repository.ProductRepository;
+import br.com.meli.PIFrescos.repository.BatchRepository;
 import br.com.meli.PIFrescos.repository.PurchaseOrderRepository;
 import br.com.meli.PIFrescos.repository.UserRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,32 +14,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.yaml.snakeyaml.tokens.Token;
+import org.springframework.test.web.servlet.MvcResult;
 
-import java.beans.Encoder;
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 /**
  * @author Antonio Hugo
  */
+
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -49,14 +42,13 @@ public class PurchaseOrderControllerIT {
     private PurchaseOrderRepository purchaseOrderRepository;
 
     @MockBean
-    private ProductRepository productRepository;
+    private UserRepository userRepository;
+
+    @MockBean
+    private BatchRepository batchRepository;
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private WebApplicationContext context;
-
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -66,30 +58,55 @@ public class PurchaseOrderControllerIT {
             + " \"cartList\": ["
             + " { "
             +       " \"batchId\": 1,"
-            +       " \"quantity\": 5"
+            +       " \"quantity\": 1"
             + " },"
             + " {"
             +     " \"batchId\": 2,"
-            +     " \"quantity\": 5"
+            +     " \"quantity\": 1"
             + " }"
             + " ]"
             + " }";
 
+    String userLogin = "{"
+            + "\"email\": \"meli@gmail.com\", "
+            + "\"password\": \"123456\""
+            + "}";
+
+    private String accessToken;
+
     PurchaseOrder purchaseOrderMock = new PurchaseOrder();
     User userMock = new User();
+    Profile profile= new Profile();
     ProductsCart productsCartMock = new ProductsCart();
     ProductsCart productsCartMock2 = new ProductsCart();
+    ProductsCart productsCartMock3 = new ProductsCart();
+    Product mockProduct = new Product();
+    Product mockProduct2 = new Product();
+
     @BeforeEach
-    public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    public void setUp() throws Exception {
+
+        profile.setId(1L);
+        profile.setName("ADMIN");
         userMock.setId(1);
         userMock.setFullname("John Doe");
         userMock.setEmail("john@mercadolivre.com.br");
-        userMock.setPassword("123456");
+        userMock.setPassword("$2a$10$GtzVniP9dVMmVW2YxytuvOG9kHu9nrwAxe8/UXSFkaECmIJ4UJcHy");
+        userMock.setProfiles(List.of(profile));
         userMock.setRole(UserRole.ADMIN);
 
+        mockProduct.setProductId(1);
+        mockProduct.setProductType(StorageType.FRESH);
+        mockProduct.setProductName("Uva");
+        mockProduct.setProductDescription("Mock description");
+
+        mockProduct2.setProductId(2);
+        mockProduct2.setProductType(StorageType.FRESH);
+        mockProduct2.setProductName("Maça");
+        mockProduct2.setProductDescription("Mock description");
+
         productsCartMock.setId(1);
-        productsCartMock.setQuantity(5);
+        productsCartMock.setQuantity(1);
         productsCartMock.setBatch(Batch.builder()
                 .batchNumber(1)
                 .currentTemperature(7.0f)
@@ -98,10 +115,12 @@ public class PurchaseOrderControllerIT {
                 .currentQuantity(50)
                 .manufacturingDate(LocalDate.now())
                 .dueDate(LocalDate.now())
+                .product(mockProduct)
+                .unitPrice(BigDecimal.valueOf(20.0))
                 .build());
 
         productsCartMock2.setId(2);
-        productsCartMock2.setQuantity(5);
+        productsCartMock2.setQuantity(1);
         productsCartMock2.setBatch(Batch.builder()
                 .batchNumber(2)
                 .currentTemperature(7.0f)
@@ -110,6 +129,22 @@ public class PurchaseOrderControllerIT {
                 .currentQuantity(50)
                 .manufacturingDate(LocalDate.now())
                 .dueDate(LocalDate.now())
+                .product(mockProduct2)
+                .unitPrice(BigDecimal.valueOf(10.0))
+                .build());
+
+        productsCartMock3.setId(2);
+        productsCartMock3.setQuantity(2);
+        productsCartMock3.setBatch(Batch.builder()
+                .batchNumber(2)
+                .currentTemperature(7.0f)
+                .minimumTemperature(6.0f)
+                .initialQuantity(10)
+                .currentQuantity(50)
+                .manufacturingDate(LocalDate.now())
+                .dueDate(LocalDate.now())
+                .product(mockProduct2)
+                .unitPrice(BigDecimal.valueOf(10.0))
                 .build());
 
         purchaseOrderMock.setId(1);
@@ -120,57 +155,213 @@ public class PurchaseOrderControllerIT {
 
     }
 
+    /**
+     * This method returned the mock user token.
+     * @return String
+     * @author Antonio Hugo
+     *
+     */
+    private String userLogin() throws Exception {
+        Mockito.when(userRepository.findByEmail(any())).thenReturn(Optional.ofNullable(userMock));
+        MvcResult result = mockMvc.perform(post("/auth")
+                .contentType("application/json").content(userLogin))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        TypeReference<TokenDto> typeReference = new TypeReference<TokenDto>() {};
+        TokenDto token = objectMapper.readValue(result.getResponse().getContentAsString(), typeReference);
+
+        return "Bearer " + token.getToken();
+    }
 
     /**
      * @author Antonio Hugo
      * Este teste espera criar um novo pedido.
      */
-//    @Test
-////    @WithMockUser(username = "John Doe", roles = { "USER", "ADMIN"}, password = "123456")
-//    @WithMockUser("test")
-//    public void shouldCreatePurchaseOrder() throws Exception {
-////        PurchaseOrderForm result = objectMapper.readValue(payload, PurchaseOrderForm.class);
-////        String token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNZWxpRnJlc2NvcyIsInN1YiI6IjQiLCJpYXQiOjE2NTExODEwNzgsImV4cCI6MTY1MTI2NzQ3OH0.K7ZCAYBcvmnLke7KwSdUDS7sBM0W8d--5-dWF1IUshQ";
-////        Mockito.when(userRepository.findAll()).thenReturn(List.of(userMock));
-//        Mockito.when(purchaseOrderRepository.save(any())).thenReturn(purchaseOrderMock);
-//        Mockito.when(purchaseOrderRepository.save(any())).thenReturn(purchaseOrderMock);
-//
-//        mockMvc.perform(get("/fresh-products")
-//                .contentType(MediaType.APPLICATION_JSON).content(payload))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.totalPrice").value(""))
-//                .andReturn();
-//    }
+    @Test
+    public void shouldCreatePurchaseOrder() throws Exception {
+        this.accessToken = this.userLogin();
+
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+        Mockito.when(purchaseOrderRepository.getPurchaseOrdersByUserIdAndOrderStatusIsOPENED(any())).thenReturn(null);
+        Mockito.when(batchRepository.findByBatchNumber(any())).thenReturn(productsCartMock.getBatch());
+        Mockito.when(batchRepository.findByBatchNumber(any())).thenReturn(productsCartMock2.getBatch());
+        Mockito.when(userRepository.findByEmail(any())).thenReturn(Optional.of(userMock));
+        Mockito.when(purchaseOrderRepository.save(any())).thenReturn(purchaseOrderMock);
+
+        mockMvc.perform(post("/fresh-products/orders")
+                .header("Authorization",  accessToken)
+                .contentType("application/json").content(payload))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.totalPrice").value(30.0))
+                .andReturn();
+    }
 
     /**
      * @author Antonio Hugo
-     * Este teste espera ser retornado todos os produtos.
+     * Este teste espera receber 400 quando o paylod for vazio.
      */
     @Test
-    @WithMockUser("test")
-    public void shouldReturnAllProducts() throws Exception {
-        Product mockProduct = new Product();
-        Product mockProduct2 = new Product();
-        mockProduct.setProductId(1);
-        mockProduct.setProductType(StorageType.FRESH);
-        mockProduct.setProductName("Uva");
-        mockProduct.setProductDescription("Mock description");
+    public void shouldCreatePurchaseOrderInvalidPayload() throws Exception {
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
 
-        mockProduct2.setProductId(3);
-        mockProduct2.setProductType(StorageType.REFRIGERATED);
-        mockProduct2.setProductName("Maça");
-        mockProduct2.setProductDescription("Mock description");
+        Mockito.when(purchaseOrderRepository.save(any())).thenReturn(purchaseOrderMock);
 
-        List<Product> products = new ArrayList<>();
-
-        products.add(mockProduct);
-        products.add(mockProduct2);
-
-        Mockito.when(productRepository.findAll()).thenReturn(products);
-
-        mockMvc.perform(get("/fresh-products/"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[*].productId").value("1"))
+        mockMvc.perform(post("/fresh-products/orders")
+                .header("Authorization", accessToken)
+                .contentType("application/json").content(""))
+                .andExpect(status().isBadRequest())
                 .andReturn();
     }
+
+    /**
+     * @author Antonio Hugo
+     * Este teste espera receber todos os produtos do pedido.
+     */
+    @Test
+    public void shouldGetProductsOnPurchaseOrder() throws Exception {
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+
+        Mockito.when(purchaseOrderRepository.findByUser(any())).thenReturn(purchaseOrderMock);
+        Mockito.when(purchaseOrderRepository.getById(any())).thenReturn(purchaseOrderMock);
+        Mockito.when(purchaseOrderRepository.findById(any())).thenReturn(Optional.ofNullable(purchaseOrderMock));
+        mockMvc.perform(get("/fresh-products/orders")
+                .header("Authorization", accessToken)
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.orderNumber").value(1))
+                .andReturn();
+    }
+
+    /**
+     * @author Antonio Hugo
+     * Este teste espera 404 quando existe produtos no pedido.
+     */
+    @Test
+    public void shouldReturn404WhenNotExistProductsOnPurchaseOrder() throws Exception {
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+
+        Mockito.when(purchaseOrderRepository.findByUser(any())).thenReturn(purchaseOrderMock);
+        Mockito.when(purchaseOrderRepository.getById(any())).thenReturn(purchaseOrderMock);
+        mockMvc.perform(get("/fresh-products/orders")
+                .header("Authorization", accessToken)
+                .contentType("application/json"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("PurchaseOrder not found"))
+                .andReturn();
+    }
+
+    /**
+     * @author Antonio Hugo
+     * Este teste espera atualizar um pedido.
+     */
+    @Test
+    public void shouldUpdatePurchaseOrder() throws Exception {
+
+        String payloadUpdate = "{ \n"
+                + " \"cartList\": ["
+                + " { "
+                +       " \"batchId\": 1,"
+                +       " \"quantity\": 1"
+                + " },"
+                + " { "
+                +       " \"batchId\": 2,"
+                +       " \"quantity\": 2"
+                + " }"
+                + " ]"
+                + " }";
+
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+
+        Mockito.when(purchaseOrderRepository.findByUserId(any())).thenReturn(Optional.ofNullable(purchaseOrderMock));
+        Mockito.when(batchRepository.findByBatchNumber(any())).thenReturn(productsCartMock.getBatch());
+        Mockito.when(batchRepository.findByBatchNumber(any())).thenReturn(productsCartMock2.getBatch());
+
+        purchaseOrderMock.setCartList(List.of(productsCartMock, productsCartMock3));
+
+        Mockito.when(userRepository.findByEmail(any())).thenReturn(Optional.of(userMock));
+        Mockito.when(purchaseOrderRepository.save(any())).thenReturn(purchaseOrderMock);
+
+        mockMvc.perform(put("/fresh-products/orders")
+                .header("Authorization",  accessToken)
+                .contentType("application/json").content(payloadUpdate))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalPrice").value(40.0))
+                .andReturn();
+    }
+
+    /**
+     * @author Antonio Hugo
+     * Este teste espera receber 404 quando não exister produtos para atualizar.
+     */
+    @Test
+    public void shouldReturn404WhenNotExistProductsOnPurchaseOrderUpdate() throws Exception {
+
+        String payloadUpdate = "{ \n"
+                + " \"cartList\": ["
+                + " { "
+                +       " \"batchId\": 1,"
+                +       " \"quantity\": 1"
+                + " },"
+                + " { "
+                +       " \"batchId\": 2,"
+                +       " \"quantity\": 2"
+                + " }"
+                + " ]"
+                + " }";
+
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+
+        Mockito.when(purchaseOrderRepository.findByUserId(any())).thenReturn(Optional.empty());
+        mockMvc.perform(put("/fresh-products/orders")
+                .header("Authorization",  accessToken)
+                .contentType("application/json").content(payloadUpdate))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("PurchaseOrder not found"))
+                .andReturn();
+    }
+
+    /**
+     * @author Antonio Hugo
+     * Este teste espera finalizar um pedido aberto.
+     */
+    @Test
+    public void shouldUpdateOrderToFinish() throws Exception {
+
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+        purchaseOrderMock.setOrderStatus(OrderStatus.FINISHED);
+        Mockito.when(purchaseOrderRepository.findById(any())).thenReturn(Optional.ofNullable(purchaseOrderMock));
+
+        mockMvc.perform(put("/fresh-products/orders/finish?idOrder=1")
+                .header("Authorization",  accessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalPrice").value(30.0))
+                .andReturn();
+    }
+
+    /**
+     * @author Antonio Hugo
+     * Este teste espera receber 404 quando tentar finalizar um pedido que não existe.
+     */
+    @Test
+    public void shouldReturn404FinishOrderNotExist() throws Exception {
+
+        this.accessToken = this.userLogin();
+        Mockito.when(userRepository.findById(any())).thenReturn(Optional.ofNullable(userMock));
+
+        Mockito.when(purchaseOrderRepository.findById(any())).thenReturn(Optional.empty());
+
+        mockMvc.perform(put("/fresh-products/orders/finish?idOrder=1")
+                .header("Authorization",  accessToken))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("PurchaseOrder not found"))
+                .andReturn();
+    }
+
 }
