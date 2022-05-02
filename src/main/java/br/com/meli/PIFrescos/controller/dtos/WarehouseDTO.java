b3d7cb7b217228f77e4793f1612b5f4e058f5559
a-preis <ana.preis@mercadolivre.com>
2022-05-02 15:49:04 -0300

    Final do Requisito 04

--- src/main/java/br/com/meli/PIFrescos/controller/dtos/WarehouseDTO.java
+++ src/main/java/br/com/meli/PIFrescos/controller/dtos/WarehouseDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,7 +16,7 @@ import java.util.stream.Collectors;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class WarehouseDTO {
+public class WarehouseDTO implements WarehouseQuantityDTO{
   private Integer warehouseCode;
   private Integer totalQuantity;
 
@@ -30,5 +31,15 @@ public class WarehouseDTO {
 
   public static List<WarehouseDTO> warehouseDTOList(List<Warehouse> warehouses, List<Batch> batches) {
     return warehouses.stream().map(warehouse -> new WarehouseDTO(warehouse, batches)).collect(Collectors.toList());
+  }
+
+  @Override
+  public Integer getWarehousecode() {
+    return warehouseCode;
+  }
+
+  @Override
+  public BigInteger getTotalquantity() {
+    return BigInteger.valueOf(totalQuantity);
   }
 }
