3b57b9c2c3a06804402804972525d25aef0b9f5e
Marcelo <marcelo.leite@mercadolivre.com>
2022-05-03 15:35:30 -0300

    feat: created sendEmail method in service

--- src/main/java/br/com/meli/PIFrescos/service/EmailService.java
+++ src/main/java/br/com/meli/PIFrescos/service/EmailService.java
@@ -1,9 +1,15 @@
 package br.com.meli.PIFrescos.service;
 
 import br.com.meli.PIFrescos.models.Email;
+import br.com.meli.PIFrescos.models.StatusEmail;
 import br.com.meli.PIFrescos.repository.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,7 +17,24 @@ public class EmailService {
   @Autowired
   private EmailRepository emailRepository;
 
-  public void sendEmail(Email email) {
+  @Autowired
+  private JavaMailSender emailSender;
 
+  public Email sendEmail(Email email) {
+    email.setSendDateEmail(LocalDateTime.now());
+    try {
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setFrom(email.getEmailFrom());
+      message.setTo(email.getEmailTo());
+      message.setSubject(email.getSubject());
+      message.setText(email.getText());
+      emailSender.send(message);
+
+      email.setStatusEmail(StatusEmail.SENT);
+    } catch (MailException e) {
+      email.setStatusEmail(StatusEmail.ERROR);
+    } finally {
+      return emailRepository.save(email);
+    }
   }
 }
