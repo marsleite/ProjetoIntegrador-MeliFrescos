dd3e19151ef2a24153bfeaaf860d3d5c5fb2b2c0
Marcelo <marcelo.leite@mercadolivre.com>
2022-05-05 10:25:48 -0300

    feat: created email template response

--- src/main/java/br/com/meli/PIFrescos/service/EmailService.java
+++ src/main/java/br/com/meli/PIFrescos/service/EmailService.java
@@ -24,7 +24,7 @@ public class EmailService implements EmailSender {
       MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
       helper.setText(email, true);
       helper.setTo(to);
-      helper.setSubject("Confirm your email");
+      helper.setSubject("Confirmação!");
       helper.setFrom("marsleite@gmail.com");
       mailSender.send(mimeMessage);
     } catch(MessagingException e) {
--- src/main/java/br/com/meli/PIFrescos/service/UserService.java
+++ src/main/java/br/com/meli/PIFrescos/service/UserService.java
@@ -41,10 +41,11 @@ public class UserService {
      * @author Marcelo Leite
      * Requisito 6
      */
-    String link = "localhost:8080/";
+    String link = "http://localhost:8080/";
+    String title = "Confirmação de registro";
     User createdUser = userRepository.save(user);
 
-    emailSender.sendEmail(createdUser.getEmail(), buildEmail(createdUser.getFullname(), link));
+    emailSender.sendEmail(createdUser.getEmail(), buildEmail(createdUser.getFullname(), link, title));
     return createdUser;
   }
 
@@ -74,7 +75,7 @@ public class UserService {
 
   }
 
-  private String buildEmail(String name, String link) {
+  private String buildEmail(String name, String link, String title) {
     return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
             "\n" +
             "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
@@ -92,7 +93,7 @@ public class UserService {
             "                  \n" +
             "                    </td>\n" +
             "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
-            "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
+            "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">" + title + "</span>\n" +
             "                    </td>\n" +
             "                  </tr>\n" +
             "                </tbody></table>\n" +
@@ -130,7 +131,7 @@ public class UserService {
             "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
             "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
             "        \n" +
-            "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 15 minutes. <p>See you soon</p>" +
+            "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Olá " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Obrigado por se registrar. Clique no link abaixo para ser redirecionado para o Mercado Fresh: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n seja bem vindo. <p>atenciosamente!</p>" +
             "        \n" +
             "      </td>\n" +
             "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
